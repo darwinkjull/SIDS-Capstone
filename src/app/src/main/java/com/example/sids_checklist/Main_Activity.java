c31e7c77517a13866c930873e637102ad50374e1
Darwin Jull <darwinkjull@gmail.com>
2023-11-21 13:54:00 -0500

    Added button functions to Main_Activity.java

--- src/app/src/main/java/com/example/sids_checklist/Main_Activity.java
+++ src/app/src/main/java/com/example/sids_checklist/Main_Activity.java
@@ -7,9 +7,31 @@ This is the main activity which allows the user to access the sleeping checklist
 
 */
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-public class Main_Activity extends AppCompatActivity implements DialogCloseListener{
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        Button goToChecklist = findViewById(R.id.goToChecklist);
+        goToChecklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navigationController = Navigation.findNavController(Main_Activity.this, R.id.nav_host_fragment);
+                navigationController.navigate(R.id.mainToChecklist);
+            }
+        });
 
 
+    }
 }
