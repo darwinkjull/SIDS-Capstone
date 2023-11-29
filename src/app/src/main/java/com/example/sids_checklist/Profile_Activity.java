b3621808fc9228d93d946e4b486bfa0f31cc9bf7
Darwin Jull <darwinkjull@gmail.com>
2023-11-28 23:30:24 -0500

    Added profile activity to manifest, implemented navigation buttons
    Added .Profile_Activity to manifest. Also added functionality to main page buttons and back button to allow for simplified navigation to/from user management page.

--- src/app/src/main/java/com/example/sids_checklist/Profile_Activity.java
+++ src/app/src/main/java/com/example/sids_checklist/Profile_Activity.java
@@ -1,13 +1,24 @@
 package com.example.sids_checklist;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.sids_checklist.checklistreports.Checklist_Reports;
+import java.util.Objects;
 
 public class Profile_Activity extends AppCompatActivity {
 
     @Override
-    protected void OnCreate(Bundle SavedInstance){
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Button returnFromProfileButton = findViewById(R.id.returnFromProfileButton);
+
+        returnFromProfileButton.setOnClickListener(v -> startActivity(new Intent(Profile_Activity.this,
+                Main_Activity.class)));
 
     }
 }
