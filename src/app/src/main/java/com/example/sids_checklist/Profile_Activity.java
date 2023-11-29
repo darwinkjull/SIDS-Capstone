10f6680c8e40cb73e0582e022ccb3e92603ca5e1
Darwin Jull <darwinkjull@gmail.com>
2023-11-28 22:36:29 -0500

    Added java, layout files for new profile management page
    Added java file to implement profile management services. Added layout page for profile management.

---
+++ src/app/src/main/java/com/example/sids_checklist/Profile_Activity.java
@@ -0,0 +1,13 @@
+package com.example.sids_checklist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void OnCreate(Bundle SavedInstance){
+
+    }
+}
