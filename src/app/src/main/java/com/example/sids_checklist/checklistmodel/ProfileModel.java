23283b320ffdd739ccb7f6b9b66e95571cc875d8
Darwin Jull <darwinkjull@gmail.com>
2023-12-29 13:23:58 +0100

    Added ProfileModel.java
    Added Profile data model to project, implemented model for two variables: id and username.

---
+++ src/app/src/main/java/com/example/sids_checklist/checklistmodel/ProfileModel.java
@@ -0,0 +1,12 @@
+package com.example.sids_checklist.checklistmodel;
+
+public class ProfileModel {
+    private int id;
+    private String username;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getUsername(){ return username; }
+    public void setUsername (String username){ this.username = username; }
+
+}
