b260195e28f501dd038d1c00249a63724ca725d8
Darwin Jull <darwinkjull@gmail.com>
2024-03-11 12:35:19 -0400

    Moved P2P methods to sharing activity
    Moved P2P attributes and methods to the sharing activity and deleted the separate P2P Manger class.

--- src/app/src/main/java/com/example/sids_checklist/Sharing_Activity.java
+++ src/app/src/main/java/com/example/sids_checklist/Sharing_Activity.java
@@ -1,6 +1,12 @@
 package com.example.sids_checklist;
 
+import static android.os.Looper.getMainLooper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sids_checklist.checklistadapter.ProfileListAdapter;
 import com.example.sids_checklist.checklistmodel.ProfileModel;
+import com.example.sids_checklist.checklistsharing.WifiDirectBroadcastReceiver;
 import com.example.sids_checklist.checklistutils.Profile_DatabaseHandler;
 
 import java.util.List;
@@ -30,6 +37,11 @@ public class Sharing_Activity extends AppCompatActivity {
     private int profileID;
     private List<ProfileModel> profileModelList;
     private ProfileListAdapter profileListAdapter;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver receiver;
+    private IntentFilter intentFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,13 +76,20 @@ public class Sharing_Activity extends AppCompatActivity {
                 sendInfoPopup.showAddProfilePopUp(v);
             }
         });
-//        receiveInfoButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Sharing_ReceiveInfo receiveInfoPopup = new Sharing_ReceiveInfo();
-//                receiveInfoPopup.showAddProfilePopUp(v);
-//            }
-//        });
+
+
+        // P2P sharing below:
+        manager = (WifiP2pManager) this.getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new WifiDirectBroadcastReceiver(manager, channel, this);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+
     }
 
 }
